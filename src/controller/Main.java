e646fb75c59bb623a9daea1b2b6347c56e3b21e1
ColgateSmile <mor.dror131@gmail.com>
2021-11-15 17:46:36 +0200

    Fixed
    Checked and run

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,8 +1,5 @@
 package controller;
 
-import java.util.ArrayList;
-
-import Tests.sysDataTester;
 import javafx.application.Application;
 
 import javafx.fxml.FXMLLoader;
@@ -10,36 +7,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import misc.MapData;
-import misc.QuestionJsonRead;
-import misc.QuestionJsonWriterEx;
-import model.QuestionObject;
+
 
 
 
 public class Main extends Application {
 
 	public static void main(String[] args) throws ClassNotFoundException {
-
-		QuestionJsonRead r = new QuestionJsonRead ();
-
-		ArrayList<QuestionObject> CheckList = r.readQuestionsFromJson();
-
-		
-		sysDataTester test = new sysDataTester();
-		test.MapData_Singleton_Check(); // this is the Jtest Call For the MapData Singleton Check//
-		test.questionDeSer_test();// this is the Jtest Call For the QuestionDeser  Check//
-		test.QuestionSer_test(CheckList);
-		
-		
-		
-		
-		
-		 
-		 
-		 
-		 /*QuestionJsonWriterEx check = new QuestionJsonWriterEx();
-		 check.serialazation(QuestionJsonRead.questionsAndAnswers);*/
 		
 		launch(args);
 
--- src/misc/QuestionJsonRead.java
+++ src/misc/QuestionJsonRead.java
@@ -1,6 +1,5 @@
 package misc;
 
-import java.io.FileReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
