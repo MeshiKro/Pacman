68af4f28d15dc11342e325f0360e6c1429b4bd4b
MeshiKro <92453687+MeshiKro@users.noreply.github.com>
2021-12-01 22:19:29 +0200

    add button

--- src/view/EditQuestionScreen.java
+++ src/view/EditQuestionScreen.java
@@ -1,8 +1,11 @@
 package view;
 
+import java.io.InputStream;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -36,45 +39,94 @@ public class EditQuestionScreen {
 
 	@FXML
 	private AnchorPane pane;
-
+	
 	@FXML
-	private ComboBox<?> themeField;
+	private ImageView adddBtn;
+	
+    @FXML
+    private ImageView saveBtn;
+    
+	@FXML
+	private ImageView backBtn;
+	
+	@FXML
+	private ComboBox<String> themeField;
 
 	@FXML
 	private Text title;
 
 	public void initialize(String screenType) {
-		
+
 		Font f = Font.loadFont(getClass().getResourceAsStream("/resources/fonts/OCRExtendedV1.ttf"), 60);
 		title.setFont(f);
 		title.setText(screenType + " Question");
-		}
-	
-	// Hover Section
+		if(screenType.equals("Add"))
+		{
+			adddBtn.setVisible(true);
+			saveBtn.setVisible(false);
 
+		}else
+		{
+			adddBtn.setVisible(false);
+			saveBtn.setVisible(true);
+		}
+	}
+
+	// Hover Section
 
 	@FXML
 	void hoverStartSideButton(MouseEvent event) {
-		String img ="buttonconinterClicked";
-		GlobalFuncations.hoverButton(getClass().getResourceAsStream("/images/" + img + ".png"), homeBtn);		
+		hoverSideButton(event.getPickResult().getIntersectedNode().getId());
 
 	}
-	
+
 	@FXML
 	void hoverEndSideButton(MouseEvent event) {
-		String img ="buttonconinter";
-		GlobalFuncations.hoverButton(getClass().getResourceAsStream("/images/" + img + ".png"), homeBtn);		
+		hoverSideButton(event.getPickResult().getIntersectedNode().getId());
+
 	}
 
+	private void hoverSideButton(String id) {
+		if(id == null)
+			return;
+		Image image = createImage("buttonconinterClicked");
+		id = id.replace("Btn", "").replace("Pane", "");
+
+		switch (id) {
+		case "home":
+			homeBtn.setImage(image);
+			break;
+		case "back":
+			backBtn.setImage(image);
+			break;		
+		default:
+			image = createImage("buttonconinter");
+			homeBtn.setImage(image);
+			backBtn.setImage(image);
+			
+		}
+
+	}
+
+	private Image createImage(String img) {
+		InputStream inStream = getClass().getResourceAsStream("/images/" + img + ".png");
+		return new Image(inStream);
+	}
 	// End Hover Section
 
 	// OnClick Section//
 
 	@FXML
 	void HomeBtnClicked(MouseEvent event) {
-		GlobalFuncations.switchScreen(pane,"MainScreen",(getClass().getResource("/view/" + "MainScreen" + ".fxml")),"");
+		GlobalFuncations.switchScreen(pane, "MainScreen", (getClass().getResource("/view/" + "MainScreen" + ".fxml")),
+				"");
 
 	}
+	 @FXML
+	    void BackBtnClicked(MouseEvent event) {
+	    	GlobalFuncations.switchScreen(pane,"QuestionScreen",(getClass().getResource("/view/" + "QuestionScreen" + ".fxml")),"");
+
+	    }
 	// End OnClick Section
 
 }
