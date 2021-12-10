16b1fc67cb9d741f84e56c32e72accccec17dfb0
MeshiKro <92453687+MeshiKro@users.noreply.github.com>
2021-12-10 15:33:40 +0200

    set question from json to question lsit screen

--- src/view/QuestionsListScreen.java
+++ src/view/QuestionsListScreen.java
@@ -1,53 +1,229 @@
 package view;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import misc.GlobalFuncations;
+import misc.JsonRead;
+import model.QuestionInJson;
 
 public class QuestionsListScreen {
 
+	@FXML
+	private ImageView backBtn;
+
+	@FXML
+	private AnchorPane backBtnPane;
+
+	@FXML
+	private ImageView backicon;
+
+	@FXML
+	private ImageView deleteBtn1;
+
+	@FXML
+	private ImageView deleteBtn2;
+
+	@FXML
+	private ImageView deleteBtn3;
+
+	@FXML
+	private ImageView deleteBtn4;
+
+	@FXML
+	private ImageView deleteBtn5;
+
+	@FXML
+	private ImageView deleteBtn6;
+
+	@FXML
+	private ImageView editBtn1;
+
+	@FXML
+	private ImageView editBtn2;
+
+	@FXML
+	private ImageView editBtn3;
+
+	@FXML
+	private ImageView editBtn4;
+
+	@FXML
+	private ImageView editBtn5;
+
+	@FXML
+	private ImageView editBtn6;
+
+	@FXML
+	private ImageView homeBtn;
+
+	@FXML
+	private AnchorPane homePane;
+
+	@FXML
+	private Label label;
+
+	@FXML
+	private Label label1;
+
+	@FXML
+	private Label label11;
+
+	@FXML
+	private Label label111;
+
+	@FXML
+	private Text levleField1;
+
+	@FXML
+	private Text levleField2;
+
+	@FXML
+	private Text levleField3;
+
+	@FXML
+	private Text levleField4;
+
+	@FXML
+	private Text levleField5;
+
+	@FXML
+	private Text levleField6;
+
+	@FXML
+	private AnchorPane nextBtnPane;
+
+	@FXML
+	private ImageView nextIcon1;
+
+	@FXML
+	private ImageView nextIcon2;
+
+	@FXML
+	private AnchorPane pane;
+
+	@FXML
+	private Text quesionField1;
+
+	@FXML
+	private Text quesionField2;
+
+	@FXML
+	private Text quesionField3;
+
+	@FXML
+	private Text quesionField4;
+
+	@FXML
+	private Text quesionField5;
+
+	@FXML
+	private Text quesionField6;
+
 
     @FXML
-    private ImageView backBtn;
+    private ImageView backiquestioncon1;
 
     @FXML
-    private AnchorPane backBtnPane;
+    private ImageView backquestionBtn1;
 
     @FXML
-    private ImageView backicon;
+    private AnchorPane backquestionBtnPane1;
+	
+	private int quesiotnIndex;
 
-    @FXML
-    private ImageView homeBtn;
+	public void initialize() {
+		JsonRead jr = new JsonRead();
+		ArrayList<QuestionInJson> array = jr.readQuestionsFromJson();
+		quesiotnIndex = 6;
+		System.out.println(" array " + array.size());
+		if(array.size() >6)
+		{
+			nextBtnPane.setVisible(true);
+			backquestionBtnPane1.setVisible(true);
+		}else
+		{
+			nextBtnPane.setVisible(false);
+			backquestionBtnPane1.setVisible(false);
+		}
+		
+		for (int i = 0; i < quesiotnIndex; i++) {
 
-    @FXML
-    private AnchorPane homePane;
+			addQuestion(array, i);
+		}
 
-    @FXML
-    private Label label;
+	}
 
-    @FXML
-    private Label label1;
+	void addQuestion(ArrayList<QuestionInJson> array, int index) {
+		System.out.println(array.get(index).getQuestion());
 
-    @FXML
-    private Label label11;
+		
+		Text field = findText("quesionField" + String.valueOf(index + 1));
+		field.setText(array.get(index).getQuestion());
+		field.setVisible(true);
+		
+		
+		field = findText("levleField" + String.valueOf(index + 1));
+		field.setText(getLevel(array.get(index).getLevel()));
+		field.setVisible(true);
 
-    @FXML
-    private Label label111;
+		
+		ImageView img = findImageView("editBtn" + String.valueOf(index + 1));
+		img.setVisible(true);
 
-    @FXML
-    private AnchorPane pane;
+		img = findImageView("deleteBtn" + String.valueOf(index + 1));
+		img.setVisible(true);
 
-    @FXML
-    private ImageView startButtonClicked;
+	}
 
-    @FXML
-    private Label title;
+	private ImageView findImageView(String id) {
+
+		for (int i = 0; i < pane.getChildren().size(); i++) {
+			Node current = pane.getChildren().get(i);
+			if (current.getId() != null && current.getId().equals(id)) {
+				return (ImageView) current;
+			}
+		}
+		return null;
+	}
+
+	private Text findText(String id) {
+
+		for (int i = 0; i < pane.getChildren().size(); i++) {
+			Node current = pane.getChildren().get(i);
+			if (current.getId() != null && current.getId().equals(id)) {
+				return (Text) current;
+			}
+		}
+		return null;
+	}
+
+	private String getLevel(String level) {
+		String lvl = "";
+
+		switch (level) {
+		case "1":
+			lvl = "Easy";
+			break;
+		case "2":
+			lvl = "Meduim";
+			break;
+		case "3":
+			lvl = "Hard";
+			break;
+		}
+		return lvl;
+
+	}
 
 	// Hover Section
 
@@ -64,7 +240,7 @@ public class QuestionsListScreen {
 	}
 
 	private void hoverSideButton(String id) {
-		if(id == null)
+		if (id == null)
 			return;
 		Image image = createImage("buttonconinterClicked");
 		id = id.replace("Btn", "").replace("Pane", "");
@@ -75,12 +251,12 @@ public class QuestionsListScreen {
 			break;
 		case "back":
 			backBtn.setImage(image);
-			break;		
+			break;
 		default:
 			image = createImage("buttonconinter");
 			homeBtn.setImage(image);
 			backBtn.setImage(image);
-			
+
 		}
 
 	}
@@ -96,16 +272,28 @@ public class QuestionsListScreen {
 
 	@FXML
 	void HomeBtnClicked(MouseEvent event) {
-    	GlobalFuncations.switchScreen(pane,"MainScreen",(getClass().getResource("/view/" + "MainScreen" + ".fxml")),"");
+		GlobalFuncations.switchScreen(pane, "MainScreen", (getClass().getResource("/view/" + "MainScreen" + ".fxml")),
+				"");
 
 	}
 
-    @FXML
-    void BackBtnClicked(MouseEvent event) {
-    	GlobalFuncations.switchScreen(pane,"QuestionScreen",(getClass().getResource("/view/" + "QuestionScreen" + ".fxml")),"");
+	@FXML
+	void BackBtnClicked(MouseEvent event) {
+		GlobalFuncations.switchScreen(pane, "QuestionScreen",
+				(getClass().getResource("/view/" + "QuestionScreen" + ".fxml")), "");
 
-    }
-	
+	}
+
+	@FXML
+	void nextQuesiotnBtnClicked(MouseEvent event) {
+
+	}
+
+	@FXML
+	void BackQuestionBtnClicked(MouseEvent event) {
+
+	}
+
 	// End OnClick Section
 
 }
